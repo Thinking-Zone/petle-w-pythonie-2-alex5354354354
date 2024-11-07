b86fd2d03eaaaaab0a29b74e9bc6e14f6c150ec6
alex5354354354 <aleks.muraw@wp.pl>
2024-11-07 14:33:13 +0100

    Update zadanie4.py

---
+++ zadanie4.py
@@ -0,0 +1,8 @@
+# Pobranie ciągu znaków od użytkownika
+input_string = input("Podaj ciąg znaków: ")
+
+# Odwrócenie ciągu
+reversed_string = input_string[::-1]
+
+# Wyświetlenie odwróconego ciągu
+print("Odwrócony ciąg znaków:", reversed_string)
